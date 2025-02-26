86ad4673f9e3cdceeaf5e55be5a4facbc0c57394
Hugues eyong <hugues777@outlook.com>
2025-02-26 10:33:14 +0100

    Amélioration du textes A propos de moi

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,85 +1,135 @@
-import './About.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import "./About.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 function About() {
   return (
     <div className="about">
-        <div className="about-content">
-            <div className="column about-left">
-                <div className="bar">
-                  <div className="info">
-                    <span>HTML</span>
-                    <span>85%</span>
-                  </div>
-                  <div className="line"><span className='html'></span></div>
-                </div>
-                <div className="bar">
-                  <div className="info">
-                    <span>CSS/SCSS</span>
-                    <span>80%</span>
-                  </div>
-                  <div className="line"><span className='css'></span></div>
-                </div>
-                <div className="bar">
-                  <div className="info">
-                    <span>JAVASCRIPT</span>
-                    <span>65%</span>
-                  </div>
-                  <div className="line"><span className='javascript'></span></div>
-                </div>
-                <div className="bar">
-                  <div className="info">
-                    <span>REACT</span>
-                    <span>60%</span>
-                  </div>
-                  <div className="line"><span className='react'></span></div>
-                </div>
-                <div className="bar">
-                  <div className="info">
-                    <span>Vue JS</span>
-                    <span>50%</span>
-                  </div>
-                  <div className="line"><span className='vuejs'></span></div>
-                </div>
-                <div className="bar">
-                  <div className="info">
-                    <span>NODEJS</span>
-                    <span>60%</span>
-                  </div>
-                  <div className="line"><span className='react'></span></div>
-                </div>
-                <div className="bar">
-                  <div className="info">
-                    <span>PHP</span>
-                    <span>80%</span>
-                  </div>
-                  <div className="line"><span className='css'></span></div>
-                </div>
-                <div className="bar">
-                  <div className="info">
-                    <span>MYSQL</span>
-                    <span>80%</span>
-                  </div>
-                  <div className="line"><span className='css'></span></div>
-                </div>
-                <div className="bar">
-                  <div className="info">
-                    <span>GIT</span>
-                    <span>80%</span>
-                  </div>
-                  <div className="line"><span className='css'></span></div>
-                </div>
+      <div className="about-content">
+        <div className="column about-left">
+          <div className="bar">
+            <div className="info">
+              <span>HTML</span>
+              <span>85%</span>
             </div>
-            <div className="column about-right">
-              <p>Actuellement, j’occupe le poste d’opérateur laminage au sein de l’entreprise ArcelorMittal Gandrange. J’aimerais évoluer dans le métier du web car ce dernier me passionne depuis plusieurs années, c’est pourquoi je me suis orienté progressivement dans cette reconversion professionnelle. </p>
-              <p>Cette volonté m’a amené à apprendre plusieurs technologies du web au travers des cours, tutoriels et articles, et lorsque je me suis senti capable de relever de nombreux défis en réalisant notamment plusieurs  applications en PHP/MySQL ;</p>
-              <p>je me suis lancé dans la formation du développeur web fullstack chez OpenClassrooms en vue d’obtenir un diplôme Bac+2, au terme de laquelle j’ai pu monter en compétences à travers huit grands projets réalisés au cours de la formation tant du  JavaScript, React et NodeJs. Ensuite j’ai eu l’opportunité d’améliorer la performance et d'optimiser le référencement SEO d’un site web en utilisant des outils tels que Wave et lighthouse. </p>
-              <p>Ce portfolio est le résultat du dernier projet développé en React, c&#39;est la representation concret de ce que j&#39;ai appris et maitrisé en vue de vous permettre d&#39;avoir une idée générale de mes compétences et j&#39;ai hâte de vous apporter cette expérience inédite. </p>
-                <a className='p_linkedin' href="https://www.linkedin.com/in/hugues-eyong-ebun-939b41337/"><FontAwesomeIcon className='icon' icon={faLinkedin} /> Linkedin</a> 
-            </div> 
+            <div className="line">
+              <span className="html"></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>CSS/SCSS</span>
+              <span>80%</span>
+            </div>
+            <div className="line">
+              <span className="css"></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>JAVASCRIPT</span>
+              <span>65%</span>
+            </div>
+            <div className="line">
+              <span className="javascript"></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>REACT</span>
+              <span>60%</span>
+            </div>
+            <div className="line">
+              <span className="react"></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>Vue JS</span>
+              <span>50%</span>
+            </div>
+            <div className="line">
+              <span className="vuejs"></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>NODEJS</span>
+              <span>60%</span>
+            </div>
+            <div className="line">
+              <span className="react"></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>PHP</span>
+              <span>80%</span>
+            </div>
+            <div className="line">
+              <span className="css"></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>MYSQL</span>
+              <span>80%</span>
+            </div>
+            <div className="line">
+              <span className="css"></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>GIT</span>
+              <span>80%</span>
+            </div>
+            <div className="line">
+              <span className="css"></span>
+            </div>
+          </div>
         </div>
+        <div className="column about-right">
+          <p>
+            Actuellement, je travaille en tant que développeur web chez ID
+            Baali. Mon objectif est de progresser dans le métier du web car
+            depuis longtemps, je suis passionné par le métier du web.
+          </p>
+          <p>
+            Cette passion m'a toujours poussé à me cultiver et à en apprendre
+            davantage autour du web à travers des formations, tutoriels et
+            avancées technologiques liées au web, afin d'atteindre cet objectif.
+          </p>
+          <p>
+            Après avoir terminé un DUT en Informatique en 2018 (diplôme non
+            validé), Je me suis tourné vers le travail indépendant en réalisant
+            des projets adaptés aux besoins spécifiques des associations et
+            d'autres particuliers. De plus, c'est en 2024 que je me suis engagé
+            de réaliser une autre formation chez OpenClassrooms en développement
+            informatique. C'est à ce moment-là que j'ai pu valider l'équivalent
+            d'un diplôme de niveau Bac+2 qui m'a permis d'améliorer mes
+            compétences, à travers huit projets majeurs réalisés pendant la
+            formation, tant du JavaScript, React et Node.js sont des langages
+            que j'apprécie énormement. Ensuite, j'ai eu l'opportunité
+            d'améliorer l'optimisation du référencement SEO des applications web
+            pour une performance optimale en recourant à des outils comme Wave,
+            lighthouse...
+          </p>
+          <p>
+            Ce portfolio est le reflet de mes compétences professionnelles,
+            affinées en React. c'est l'expertise que j'ai acquise dans le but de
+            de vous donner un aperçu général de mon et j'ai tellement hâte de
+            vous apporter cette expérience inédite.
+          </p>
+          <a
+            className="p_linkedin"
+            href="https://www.linkedin.com/in/hugues-eyong-ebun-939b41337/"
+          >
+            <FontAwesomeIcon className="icon" icon={faLinkedin} /> Linkedin
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,34 +1,64 @@
-import './Header.scss';
-import  { Cursor, useTypewriter } from 'react-simple-typewriter'
+import "./Header.scss";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 // import {cvhugues} from 'https://secoursdeleternel.fr/images/portfolio-hugues/pdf/cv-hugues-eyong-dev.pdf'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   const [text] = useTypewriter({
-    words: ['Fullstack','PHP'],
-    loop:0,
-    typeSpeed:120
-  })
+    words: ["Fullstack", "PHP"],
+    loop: 0,
+    typeSpeed: 120,
+  });
 
   return (
     <div className="header">
       <div className="header-content">
         <div className="main">
-          <h1>Bonjour !  Je suis Hugues EYONG</h1>
-          <h2>Developpeur web <span>{text} <Cursor/></span></h2>
-          <p>Passionné du développement<span> web et ses technologies</span>. je suis en reconversion <span>professionnelle et à la recherche des nouvelles</span> opportunités <span>pour  concilier mes acquis en frontend et backend </span> ayant déjà <span>réaliser des projets concrets en PHP, React, Vue JS, Javascript et Nodejs(Express).</span> </p>
-        
+          <h1>Bonjour ! Je suis Hugues EYONG</h1>
+          <h2>
+            Developpeur web{" "}
+            <span>
+              {text} <Cursor />
+            </span>
+          </h2>
+          <p>
+            Passionné du développement<span> web et ses technologies</span>. je
+            suis activement à la recherche des nouvelles opportunités{" "}
+            <span>pour concilier mes acquis en frontend et backend </span> ayant
+            déjà{" "}
+            <span>
+              travailler et collaborer dans divers projets en PHP, React,
+              Javascript et Nodejs(Express).
+            </span>{" "}
+          </p>
+
           <div className="btn-group">
-            <a className='voir' href="https://github.com/hugues77" target='blank'><FontAwesomeIcon icon={faGithub} className='icon' /> Voir mon github </a>
+            <a
+              className="voir"
+              href="https://github.com/hugues77"
+              target="blank"
+            >
+              <FontAwesomeIcon icon={faGithub} className="icon" /> Voir mon
+              github{" "}
+            </a>
             {/* <a className='download' href= '#'><FontAwesomeIcon icon={faDownload} className='icon' />Télécharger mon CV </a> */}
-            <a  href="https://secoursdeleternel.fr/images/portfolio-hugues/pdf/cv-hugues-eyong-dev.pdf" target='blank'  className='download_btn' alt="cv hugues eyong dev" download><FontAwesomeIcon icon={faDownload} className='icon' />Télécharger mon CV </a>
+            <a
+              href="https://secoursdeleternel.fr/images/portfolio-hugues/pdf/cv-hugues-eyong-dev.pdf"
+              target="blank"
+              className="download_btn"
+              alt="cv hugues eyong dev"
+              download
+            >
+              <FontAwesomeIcon icon={faDownload} className="icon" />
+              Télécharger mon CV{" "}
+            </a>
           </div>
         </div>
-      </div> 
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
